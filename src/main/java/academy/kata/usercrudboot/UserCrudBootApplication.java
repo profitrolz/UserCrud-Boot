81c7a71593a9377f80dd0be3745cd3a65091285f
Lexa <a.pisarenko9001@gmail.com>
2022-05-08 22:45:44 +0700

    Initial commit

---
+++ src/main/java/academy/kata/usercrudboot/UserCrudBootApplication.java
@@ -0,0 +1,13 @@
+package academy.kata.usercrudboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCrudBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCrudBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/academy/kata/usercrudboot/UserCrudBootApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.kata.usercrudboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
